e40603e422c30b042dcfb4b4778dc2d0b9029990
maksimbutakov59 <maksimbutakov59@gmail.com>
2022-05-30 16:57:01 +0500

    .

--- OOP2/Source.cpp
+++ OOP2/Source.cpp
@@ -93,10 +93,16 @@ private:
 class Flat: public Human {
 public:
 	Flat(){}
-	Flat(Human *tenant, string address, float area){
-		*_tenant = *tenant;
+	Flat(Human tenant, string address, float area){
+		_tenant = tenant;
 		_address = address;
 		_area = area;
+	}
+	string getSurName() const {
+		return _tenant.getSurName();
+	}
+	string getName() const {
+		return _tenant.getName();
 	}
 	void setAddress(string address){
 		_address = address;
@@ -110,10 +116,16 @@ public:
 	double getArea(){
 		return _area;
 	}
-
+	void printFlatList(Flat flat_list[], int len) {
+		cout << "Список жильцов:\n\n";
+		for (int i = 0; i < len; i++) {
+			cout << i + 1 << ". " << flat_list[i].getSurName() << " " << flat_list[i].getName();
+			cout << "\nАдресс: " << flat_list[i].getAddress() << "\nПлощадь: " << flat_list[i].getArea() << "\n\n";
+		}
+	}
 
 private:
-	Human *_tenant = _tenant;
+	Human _tenant;
 	string _address;
 	double _area = 0;
 
@@ -123,19 +135,23 @@ int main() {
 	setlocale(LC_ALL, "Russian");
 	
 	Human human1("Бутаков", "Максим", "Александрович", 15);
-	Human* pHuman = &human1;
+	Human human2("Bob", "Smith", " ", 20);
+	Human human3("Alex", "Stone", " ", 29);
 	Student student1(human1, 10.25, "BV111", "Разработка программного обеспечения");
 	Flat flat_list[3];
+	Flat flat(human1, "fsff", 20);
 	
 	cout << "Студент\n\nФИО: " << student1.getSurName()
 	<< " " << student1.getName() << " " << student1.getPatronymic()
 	<< "\nВозраст: " << student1.getAge() << "\nУспеваемость: " << student1.getGrades()
-	<< "\nГруппа: " << student1.getGroup() << "\nНаправление: " << student1.getDirection();
+	<< "\nГруппа: " << student1.getGroup() << "\nНаправление: " << student1.getDirection() << "\n\n";
 
-	flat_list[0] = Flat(pHuman, "Groove Street, 25", 20.25);
+	flat_list[0] = Flat(human1, "№121", 20.25);
+	flat_list[1] = Flat(human2, "№122", 22);
+	flat_list[2] = Flat(human3, "№123", 25.12);
 
-	cout << "\n\nКвартира\n\nВладелец: " << flat_list[0].getSurName() << " " << flat_list[0].getName();
-	cout << "\nАдресс: " << flat_list[0].getAddress() << "\nПлощадь: " << flat_list[0].getArea();
+	flat.printFlatList(flat_list, 3);
+	
 
 	
 
